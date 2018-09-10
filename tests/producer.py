528ebbab01501dddd931ac1a17ea83e54341a3da
r.vishnevskiy <r.vishnevskiy@tinkoff.ru>
2018-09-10 10:34:57 +0700

    Docker with spring-boot

--- tests/producer.py
+++ tests/producer.py
@@ -7,7 +7,7 @@ if len(sys.argv) != 4:
 	print('3 arguments required: product, source and price');
 	exit(-1);
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'), api_version=(0, 10))
 
 future = producer.send('topic', key=b'1111', value={'product': sys.argv[1], 'source': sys.argv[2], 'price': sys.argv[3]})
 future.get(timeout=60)
